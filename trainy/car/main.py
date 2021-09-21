a4d29d1b355f8192a8a43573c829d7cd6ee78442
koalcx <lisicun.aleksandr@gmail.com>
2021-09-21 06:53:08 +0300

    car2

--- trainy/car/main.py
+++ trainy/car/main.py
@@ -1,43 +1,62 @@
 from abc import ABC, abstractmethod
 
+'''
+    У нас появляются типы двигателей
+    Средняя скорость у разных типов разная.
+'''
 
-class Car(ABC):
-    def __init__(self, brand, optimal_speed):
-        self.brand = brand
+
+class Engine(ABC):
+    def __init__(self, optimal_speed):
         self.optimal_speed = optimal_speed
 
 
+class Diesel(Engine):
+    pass
+
+
+class Benzine(Engine):
+    pass
+
+
+class Car(ABC):
+
+    def __init__(self, brand, engine: Engine):
+        self.brand = brand
+        self.engine = engine
+
+
 class Distance:
-    def __init__(self, car):
+    def __init__(self, car: Car):
         self.car = car
 
     def distance(self, km):
-        return (km / self.car.optimal_speed) * 60
+        return (km / self.car.engine.optimal_speed) * 60
 
 
 class BMW310(Car):
-    def __init__(self):
-        super().__init__('BMW310', 110)
+    def __init__(self, engine):
+        super().__init__('BMW310', engine)
 
 
 class BMW320(Car):
-    def __init__(self):
-        super().__init__('BMW320', 120)
+    def __init__(self, engine):
+        super().__init__('BMW320', engine)
 
 
 class BMW330(Car):
-    def __init__(self):
-        super().__init__('BMW330', 130)
+    def __init__(self, engine):
+        super().__init__('BMW330', engine)
 
 
 class BMW340(Car):
-    def __init__(self):
-        super().__init__('BMW340', 140)
+    def __init__(self, engine):
+        super().__init__('BMW340', engine)
 
 
 class BMW350(Car):
-    def __init__(self):
-        super().__init__('BMW350', 150)
+    def __init__(self, engine):
+        super().__init__('BMW350', engine)
 
 
 def calc_distance(dis: Distance, distance: int):
@@ -45,24 +64,23 @@ def calc_distance(dis: Distance, distance: int):
 
 
 def main():
-    # bmw = BMW('BMW', 110)
-    # mercedes = Mercedes('Mercedes', 120)
-    # print(bmw.distance(1000))
-    # print(mercedes.distance(1000))
 
-    bmw310 = Distance(BMW310())
+    d = Diesel(123)
+    b = Benzine(321)
+
+    bmw310 = Distance(BMW310(d))
     calc_distance(bmw310, 1000)
 
-    bmw320 = Distance(BMW320())
+    bmw320 = Distance(BMW320(b))
     calc_distance(bmw320, 2000)
 
-    bmw330 = Distance(BMW330())
+    bmw330 = Distance(BMW330(d))
     calc_distance(bmw330, 3000)
 
-    bmw340 = Distance(BMW340())
+    bmw340 = Distance(BMW340(b))
     calc_distance(bmw340, 4000)
 
-    bmw350 = Distance(BMW350())
+    bmw350 = Distance(BMW350(d))
     calc_distance(bmw350, 5000)
 
 
