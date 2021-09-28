868f3a1137d230282cd82830d6c6fdf109e46444
koalcx <lisicun.aleksandr@gmail.com>
2021-09-28 12:01:09 +0300

    002

--- try.py
+++ try.py
@@ -1,3 +1,23 @@
-while True:
-    text = input('Leter: ')
-    print(text)
+import sys
+import select
+import tty
+import termios
+
+def isData():
+    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
+old_settings = termios.tcgetattr(sys.stdin)
+
+try:
+    tty.setcbreak(sys.stdin.fileno())
+
+    while True:
+
+        if isData():
+            c = sys.stdin.read(1)
+            if c == '\x1b':         # x1b is ESC
+                break
+            print(c)
+
+finally:
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
