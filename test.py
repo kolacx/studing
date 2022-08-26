88aa51fca420e435c0c8b153e236c95a0f89105a
koalcx <lisicun.aleksandr@gmail.com>
2022-08-26 09:47:31 +0300

    idn

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
+
+class A:
     pass
+
+
+class B(A):
+    pass
+
+class C(A):
+    pass
+
+
+if __name__ == "__main__":
+    b = B()
+    c = C()
+
+    i = isinstance(b, A)
+    print(i)
+
